a69ddddf97498461149a0f8ee5103e201d7d5af4
Magnus Krøyer <mkroy@netcompany.com>
2022-12-01 11:04:53 +0100

    Day 1 2022 done

---
+++ AdventOfCodeFoundation/Solvers/2022/Day1Solver2022.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCodeFoundation.IO;
+
+namespace AdventOfCodeFoundation.Solvers._2022
+{
+    [Solves("2022/12/1")]
+
+    public class Day1Solver2022 : ISolver
+    {
+        public async Task<string> SolvePartOne(Input input)
+        {
+            var lines = await input.GetInputLines();
+
+            var currentMax = -99;
+            var sumPrElf = 0;
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    if (sumPrElf > currentMax)
+                    {
+                        currentMax = sumPrElf;
+                    }
+                    sumPrElf = 0;
+                    continue;
+                }
+
+                sumPrElf += Int32.Parse(line);
+            }
+
+            return currentMax+"";
+
+        }
+
+        public async Task<string> SolvePartTwo(Input input)
+        {
+
+            var lines = await input.GetInputLines();
+
+            var sumPrElf = 0;
+            var liste = new List<int>();
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    liste.Add(sumPrElf);
+                    sumPrElf = 0;
+                    continue;
+                }
+
+                sumPrElf += Int32.Parse(line);
+            }
+
+            liste.Sort();
+            var result = liste[liste.Count - 1] + liste[liste.Count - 2] + liste[liste.Count - 3]+"";
+            return result;
+
+        }
+    }
+}
