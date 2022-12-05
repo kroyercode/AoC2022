31d2e6e9c3e51c6f4228828c70a7c32b5a858853
Magnus Krøyer <mkroy@netcompany.com>
2022-12-05 16:15:01 +0100

    Finish day 3 and day 4

---
+++ AdventOfCodeFoundation/Solvers/2022/Day3Solver2022.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCodeFoundation.IO;
+
+namespace AdventOfCodeFoundation.Solvers._2022
+{
+    [Solves("2022/12/3")]
+
+    public class Day3Solver2022 : ISolver
+    {
+        private String lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+        private String uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public async Task<string> SolvePartOne(Input input)
+        {
+            var result = 0;
+            var lines = await input.GetInputLines();
+            foreach (var line in lines)
+            {
+                var firstHalf = line.Substring(0, line.Length / 2);
+                var secondHalf = line.Substring(line.Length / 2);
+                Console.WriteLine("line.length: " + line.Length);
+                Console.WriteLine("first half: " + firstHalf + " length: " + firstHalf.Length);
+                Console.WriteLine("second half: " + secondHalf + " length: " + secondHalf.Length);
+                foreach (var letter in firstHalf)
+                {
+                    if (secondHalf.Contains(letter))
+                    {
+                        var charMapped = MapCharacterToValue(letter);
+                        Console.WriteLine("Char mapped: " + letter + " -> " + charMapped);
+                        result += charMapped;
+                        break;
+                    }
+                }
+                Console.WriteLine();
+
+            }
+
+            return result+"";
+
+        }
+
+      
+
+        public async Task<string> SolvePartTwo(Input input)
+        {
+            var lines = input.GetInputLines().Result.ToArray();
+            var result = 0;
+
+            char vaerdiSat = Char.MaxValue;
+
+            for (int i=0; i < lines.Count(); i=i+3)
+            {
+                var firstline = lines[i];
+                var secondline = lines[i+1];
+                var thirdline = lines[i+2];
+
+                vaerdiSat = Char.MaxValue; //nul stil vaerdien
+
+                foreach (var firstline_character in firstline)
+                {
+                    foreach (var secondline_character in secondline)
+                    {
+                        foreach (var thirdline_character in thirdline)
+                        {
+                            if (firstline_character == secondline_character && firstline_character == thirdline_character && vaerdiSat != firstline_character)
+                            {
+                                var charMapped = MapCharacterToValue(thirdline_character);
+                                result += charMapped;
+                                vaerdiSat = firstline_character;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            return result+"";
+        }
+
+        private int MapCharacterToValue(char character)
+        {
+            for (int i = 0; i < lowercaseAlphabet.Length; i++)
+            {
+                if (character == lowercaseAlphabet[i])
+                {
+                    return i+1;
+                }
+
+            }
+
+            var uppercaseCounter = 27;
+            for (int i = 0; i < uppercaseAlphabet.Length; i++)
+            {
+                if (character == uppercaseAlphabet[i])
+                {
+                    return uppercaseCounter;
+                }
+                uppercaseCounter++;
+            }
+
+
+            throw new ArgumentException("Bru");
+        }
+    }
+}
---
+++ AdventOfCodeFoundation/Solvers/2022/Day4Solver2022.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCodeFoundation.IO;
+
+namespace AdventOfCodeFoundation.Solvers._2022
+{
+    [Solves("2022/12/4")]
+
+    public class Day4Solver2022 : ISolver
+    {
+
+        public async Task<string> SolvePartOne(Input input)
+        {
+            var lines = await input.GetInputLines();
+            int result = 0;
+
+            foreach (var line in lines)
+            {
+                var splittedLine = line.Split(',');
+                var firstElf = splittedLine[0];
+                var secondElf = splittedLine[1];
+
+                var firstElfRooms = firstElf.Split("-");
+                var firstElfStart = Int32.Parse(firstElfRooms[0]);
+                var firstElfEnd = Int32.Parse(firstElfRooms[1]);
+
+                var secondElfRooms = secondElf.Split("-");
+                var secondElfStart = Int32.Parse(secondElfRooms[0]);
+                var secondElfEnd = Int32.Parse(secondElfRooms[1]);
+
+
+                //Hvis den anden elf ikke skal arbejde (han er totalt overlappet af første elf)
+                if (firstElfStart <= secondElfStart && firstElfEnd >= secondElfEnd)
+                {
+                    result++;
+                    continue;
+                }
+
+                //Hvis den første elf ikke skal arbejde (han er totalt overlappet af anden elf)
+                if (secondElfStart <= firstElfStart && secondElfEnd >= firstElfEnd)
+                {
+                    result++;
+                    continue;
+                }
+
+            }
+
+            return result+"";
+
+        }
+
+        public async Task<string> SolvePartTwo(Input input)
+        {
+            var lines = await input.GetInputLines();
+            int result = 0;
+
+            foreach (var line in lines)
+            {
+                var splittedLine = line.Split(',');
+                var firstElf = splittedLine[0];
+                var secondElf = splittedLine[1];
+
+                var firstElfRooms = firstElf.Split("-");
+                var firstElfStart = Int32.Parse(firstElfRooms[0]);
+                var firstElfEnd = Int32.Parse(firstElfRooms[1]);
+
+                var secondElfRooms = secondElf.Split("-");
+                var secondElfStart = Int32.Parse(secondElfRooms[0]);
+                var secondElfEnd = Int32.Parse(secondElfRooms[1]);
+
+                if (firstElfStart <= secondElfStart && firstElfEnd >= secondElfStart)
+                {
+                    result++;
+                    continue;
+                }
+
+                if (firstElfStart >= secondElfStart && firstElfEnd >= secondElfEnd )
+                {
+                    result++;
+                    continue;
+                }
+
+            }
+
+            return result + "";
+        }
+    }
+}
